8c84a9b87ffcd7df8d04f15be7cd8efc68ab778f
artrellmiller <artrellmiller@localhost>
2023-02-23 12:39:22 -0600

    Flag done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,22 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 8);
 		
+		StdDraw.clear(); {
+			
+	
+			StdDraw.setPenColor(255, 153, 204);
+	StdDraw.filledRectangle(8, 6, 6, 1);
+	
+	StdDraw.setPenColor(255, 0, 0);
+	StdDraw.filledRectangle(8, 4, 6, 1);
+	
+	StdDraw.setPenColor(0, 0, 0);
+	StdDraw.filledRectangle(8, 2, 6, 1);
+
+			}
 	}
-}
+	}
